77c9d414c405be9a950c85b8b7922b02aac0b0f9
lijunray <lijunray@me.com>
2016-03-24 21:55:15 -0400

    Simple Voice Recognition Functionality

--- app/src/main/java/us/nijikon/livelylauncher/Launcher.java
+++ app/src/main/java/us/nijikon/livelylauncher/Launcher.java
@@ -1,16 +1,116 @@
 package us.nijikon.livelylauncher;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Launcher extends Activity {
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+    private static final String TAG = Launcher.class.getName();
+
+    TextView textView;
+
+    SpeechRecognizer speechRecognizer;
+
+    Intent speechIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        textView = (TextView) findViewById(R.id.textview_display_result);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_RECORD_AUDIO: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    @Override
+    protected void onStart () {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        SpeechListener speechLitsener = new SpeechListener();
+        speechRecognizer.setRecognitionListener(speechLitsener);
+        speechIntent = new Intent();
+
+        speechIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,"us.nijikon.livelylauncher");
+
+        // Given an hint to the recognizer about what the user is going to say
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        // Specify how many results you want to receive. The results will be sorted
+        // where the first result is the one with higher confidence.
+        speechIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 20);
+
+
+        speechIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
+
+        super.onStart();
+    }
+
+    public void beginSpeaking (View view) {
+        speechRecognizer.startListening(speechIntent);
     }
 
     @Override
@@ -34,4 +134,54 @@ public class Launcher extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    class SpeechListener implements RecognitionListener {
+        public void onBufferReceived(byte[] buffer) {
+            Log.d(TAG, "buffer recieved ");
+        }
+        public void onError(int error) {
+            //if critical error then exit
+            if(error == SpeechRecognizer.ERROR_CLIENT || error == SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS){
+                Log.d(TAG, "client error");
+            }
+            //else ask to repeats
+            else{
+                Log.d(TAG, "other error");
+                speechRecognizer.startListening(speechIntent);
+            }
+        }
+        public void onEvent(int eventType, Bundle params) {
+            Log.d(TAG, "onEvent");
+        }
+        public void onPartialResults(Bundle partialResults) {
+            Log.d(TAG, "partial results");
+        }
+        public void onReadyForSpeech(Bundle params) {
+            Log.d(TAG, "on ready for speech");
+        }
+        public void onResults(Bundle results) {
+            Log.d(TAG, "on results");
+            ArrayList<String> matches = null;
+            if (results != null) {
+                matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if (matches != null) {
+                    textView.setText(matches.get(0));
+                    Log.d(TAG, matches.toString());
+                }
+
+            }
+        }
+        public void onRmsChanged(float rmsdB) {
+            //			Log.d(TAG, "rms changed");
+        }
+        public void onBeginningOfSpeech() {
+            Log.d(TAG, "speach begining");
+        }
+        public void onEndOfSpeech() {
+            Log.d(TAG, "speach done");
+        }
+
+    }
 }
+
+
